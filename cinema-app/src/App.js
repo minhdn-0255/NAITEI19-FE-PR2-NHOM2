d4d0b6cc633d7c48516d4c6ff90bad0bc182393f
minhdn-0255 <108458832+minhdn-0255@users.noreply.github.com>
2023-09-22 15:24:12 +0700

    create header footer

--- cinema-app/src/App.js
+++ cinema-app/src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
-    <div className="App">
-      TEST REACT
-      BASEeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+    </Routes>
   );
 }
 
